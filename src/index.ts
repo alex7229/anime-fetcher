519015e207a8f0fd57e0221f04cc4ea244edbb78
tup1tsa <natorvano@gmail.com>
2019-07-08 17:29:40 +0300

    run all episodes download one by one

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import downloadEpisode from "./downloadEpisode";
+import downloadEpisode from "./downloadEpisode";
 import * as puppeteer from "puppeteer";
 import parseEpisodesListPage from "./parser/parseEpisodesListPage";
 import getDownloadedEpisodesList from "./getDownloadedEpisodesList";
@@ -9,23 +9,24 @@ import parseHostingPage from "./parser/parseHostingPage";
   const downloadedEpisodes = await getDownloadedEpisodesList();
   const browser = await puppeteer.launch();
   const allEpisodesInfo = await parseEpisodesListPage(browser);
-  const newEpisodesInfo = allEpisodesInfo.filter(
-    info => !downloadedEpisodes.includes(info.episodeNumber)
-  );
-  const kissanimeEpisodePageUrl = newEpisodesInfo[22].url;
-  const episodeHostingPageUrl = await parseEpisodePage(
-    kissanimeEpisodePageUrl,
-    browser
-  );
-  const actualVideoUrl = await parseHostingPage(browser, episodeHostingPageUrl);
-  console.log(kissanimeEpisodePageUrl);
-  console.log(episodeHostingPageUrl);
-  console.log(actualVideoUrl);
-  await browser.close();
+  const newEpisodesInfo = allEpisodesInfo
+    .filter(info => !downloadedEpisodes.includes(info.episodeNumber))
+    .reverse();
 
-  // await downloadEpisode({
-  //   url:
-  //     "https://www348.playercdn.net/87/1/fJRElB8LujoBW4zisOsVkw/1562605034/190125/741FZJB9AY5V4YUPMJ8DS.mp4",
-  //   episodeNumber: 422
-  // });
+  for (const episodeInfo of newEpisodesInfo) {
+    const episodeHostingPageUrl = await parseEpisodePage(
+      episodeInfo.url,
+      browser
+    );
+    const actualVideoUrl = await parseHostingPage(
+      browser,
+      episodeHostingPageUrl
+    );
+    await downloadEpisode({
+      url: actualVideoUrl,
+      episodeNumber: episodeInfo.episodeNumber
+    });
+  }
+
+  await browser.close();
 })();
