724af6b386373c9aff375de8329a7280d54e9c20
tup1tsa <natorvano@gmail.com>
2019-07-08 15:47:26 +0300

    remove ignored .js files

--- dist/pages/getEpisodesListPage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getEpisodesListPage = async (browser) => {
-    const page = await browser.newPage();
-    await page.goto("https://kissanime.ru/Anime/Naruto-Shippuuden-Dub");
-    await page.waitFor("a.bigChar");
-    return page;
-};
-exports.default = getEpisodesListPage;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-import downloadEpisod from "./downloadEpisode";
-
-// import getEpisodesListPage from "./pages/getEpisodesListPage";
-// import * as puppeteer from "puppeteer";
-// import getEpisodesInfo from "./parser/getEpisodesInfo";
+import getEpisodesListPage from "./pages/getEpisodesListPage";
+import * as puppeteer from "puppeteer";
+import getEpisodesInfo from "./parser/getEpisodesInfo";
 
 (async () => {
-  // const browser = await puppeteer.launch();
-  // const episodesPage = await getEpisodesListPage(browser);
-  // const episodesInfo = await getEpisodesInfo(episodesPage, 320);
-  // console.log(episodesInfo);
-  // await browser.close();
-  await downloadEpisod({
-    url:
-      "https://www348.playercdn.net/87/1/fJRElB8LujoBW4zisOsVkw/1562605034/190125/741FZJB9AY5V4YUPMJ8DS.mp4",
-    episodeNumber: 422
-  });
-  console.log("download was finished");
+  const browser = await puppeteer.launch();
+  const episodesPage = await getEpisodesListPage(browser);
+  const episodesInfo = await getEpisodesInfo(episodesPage, 320);
+  console.log(episodesInfo);
+  await browser.close();
+  // await downloadEpisod({
+  //   url:
+  //     "https://www348.playercdn.net/87/1/fJRElB8LujoBW4zisOsVkw/1562605034/190125/741FZJB9AY5V4YUPMJ8DS.mp4",
+  //   episodeNumber: 422
+  // });
 })();
